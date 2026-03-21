ad2d316b6eb0fadb400d9928f2554af6a9e641ff
LorenzoCasal <lorenzocasal31@gmail.com>
2026-03-21 00:24:05 -0300

    The rooms section was created, with the addition of a carousel. Furthermore, a JSON file was created to manage the information for these rooms.

--- src/features/rooms/Rooms.jsx
+++ src/features/rooms/Rooms.jsx
@@ -1,4 +1,66 @@
-export default function Rooms () {
+import { useState, useEffect } from "react";
+import BookButton from '../../shared/components/BookButton';
+import rooms from "./data/rooms.json";
+
+export default function Rooms() {
+    const images = [
+        "/src/assets/oceansuite.png",
+        "/src/assets/aristocratsretreat.png",
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const next = () => {
+        setCurrentIndex((prev) =>
+            prev === images.length - 1 ? 0 : prev + 1
+        );
+    };
+
+    const prev = () => {
+        setCurrentIndex((prev) =>
+            prev === 0 ? images.length - 1 : prev - 1
+        );
+    };
+
+    useEffect(() => {
+        const interval = setInterval(next, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return <>
+        <main>
+            <div className="w-full relative overflow-hidden text-center">
+
+                <article className="shadows h-screen w-full flex flex-col flex-nowrap justify-center items-center absolute text-white z-10">
+                    <h1 className="text-[clamp(4rem,8vw,8rem)] font-bold leading-32 max-sm:leading-16">{rooms[currentIndex]?.room}</h1>
+                    <p className="text-[clamp(0.8rem,2vw,1.2rem)] font-light w-10/12">{rooms[currentIndex]?.descrip}</p>
+                    <BookButton url="/room/" clase="book-button" text="BOOK NOW" />
+                </article>
+
+                <div style={{ display: "flex", transform: `translateX(-${currentIndex * 100}%)`, transition: "transform 0.5s ease", zIndex: "-10" }}>
+                    {images.map((img, i) => (
+                        <img
+                            key={i}
+                            src={img}
+                            style={{
+                                width: "100%",
+                                height: "100vh",
+                                flexShrink: 0
+                            }}
+                        />
+                    ))}
+                </div>
+
+
+                <span className="absolute bottom-0 left-auto right-1/2">
+                    <button onClick={prev}>○</button>
+                    <button onClick={next}>○</button>
+                </span>
+
+            </div>
+
+
+        </main>
     </>
 }
